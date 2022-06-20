83382e07025919368039ab925f18b557243f6b27
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-06-20 21:02:53 +0700

    foreach solutions

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -2,7 +2,6 @@ package pro.sky;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class Main {
 
@@ -11,23 +10,41 @@ public class Main {
     public static void task1() {
         int[] arr = generate();
         System.out.println(Arrays.toString(arr));
-        System.out.printf("Sum: %d",
-                IntStream.of(arr).sum());
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.printf("Sum: %d", sum);
     }
 
     public static void task2() {
         int[] arr = generate();
         System.out.println(Arrays.toString(arr));
-        System.out.printf("Min: %d, Max: %d",
-                IntStream.of(arr).min().orElseThrow(),
-                IntStream.of(arr).max().orElseThrow());
+        int min = Integer.MAX_VALUE;
+        for (int i : arr) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.printf("Min: %d, Max: %d", min, max);
     }
 
     public static void task3() {
         int[] arr = generate();
         System.out.println(Arrays.toString(arr));
-        System.out.printf("Average: %.2f",
-                IntStream.of(arr).average().orElseThrow());
+        long sum = 0;
+        long count = 0;
+        for (int i : arr) {
+            sum += i;
+            count++;
+        }
+        System.out.printf("Average: %.2f", (double) sum / count);
     }
 
     public static void task4() {
@@ -36,6 +53,11 @@ public class Main {
     }
 
     private static int[] generate() {
-        return IntStream.of(new int[30]).map(i -> RNG.nextInt(100_000, 200_001)).toArray();
+        int[] arr = new int[30];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = RNG.nextInt(100_000, 200_001);
+        }
+        return arr;
     }
 }
